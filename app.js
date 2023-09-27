f4ef7430c1baf3833bd95ed920715ac1b12e2ba7
quy_le_van <quy_le_van@bita.jp>
2023-09-27 10:57:35 +0700

    feat(db): connect mongodb

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const path = require('path');
 // import Router file
 const pageRouter = require('./routers/index');
 
 const app = express();
 const http = require("http").Server(app);
+dotenv.config({ path: './config.env' });
 
 //For set layouts of html view
 app.set('views', path.join(__dirname, 'views'));
@@ -13,6 +16,18 @@ app.set('view engine', 'ejs');
 
 // Define All Route 
 pageRouter(app);
+
+// DB connection
+const DB = process.env.DATABASE_LOCAL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 http.listen(3000, function () {
   console.log('listening on *:3000');
---
+++ models/postModel.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const postSchema = new mongoose.Schema(
+  {}
+);
