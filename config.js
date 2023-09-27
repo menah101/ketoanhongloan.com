6e98861a8f0f924882d44e398a6f6968a083415b
quy_le_van <quy_le_van@bita.jp>
2023-09-27 15:38:23 +0700

    feat(user): api CRUD user

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express app
 const app = express();
 
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
